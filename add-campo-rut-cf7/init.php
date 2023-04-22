c5b01fe28e8c4d6178d1cd8592d5cde26f013361
Nejer <nejerludovid@gmail>
2023-04-22 06:38:14 -0500

    Create init.php

---
+++ add-campo-rut-cf7/init.php
@@ -0,0 +1,7 @@
+<?php
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+if (!defined('WPCF7RF_PLUGIN')) define( 'WPCF7RF_PLUGIN', __FILE__ );
+if (!defined('WPCF7RF_PLUGIN_DIR')) define( 'WPCF7RF_PLUGIN_DIR', untrailingslashit( dirname( WPCF7RF_PLUGIN ) ) );
+
+require_once WPCF7RF_PLUGIN_DIR.'/cf7rut.php';
