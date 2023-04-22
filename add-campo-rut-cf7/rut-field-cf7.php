38418c6350f02c693a43fb73c929b1f87e9d1609
Nejer <nejerludovid@gmail>
2023-04-22 06:38:23 -0500

    Create rut-field-cf7.php

---
+++ add-campo-rut-cf7/rut-field-cf7.php
@@ -0,0 +1,28 @@
+<?php
+/**
+Plugin Name: Campo RUT para CF7
+Plugin URI: http://nvalenzuela.me/
+Description: Agrega un campo de tipo RUT (Chileno) a Contact Form 7. Este plugin depende de Contact Form 7.
+Author: Nelson Valenzuela
+Version: 1.4
+Author URI: https://nvalenzuela.me/
+ */
+
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+
+require_once 'init.php';
