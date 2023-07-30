73a82e5b9caaafbd0a2e756af3c17b312c789b89
Nejer <nejerludovid@gmail>
2023-07-30 18:49:50 -0500

    Create __init__.py

---
+++ sync_pos_coupon_ent/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+
+from . import sale_coupon_program
+from . import sale_coupon
+from . import pos_order
+from . import pos_config
